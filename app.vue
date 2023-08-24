c543df9be5d73e0e019678bbcce7ddef34083d4f
Joshua <afekurojoshua@gmail.com>
2023-08-24 11:58:07 +0100

    add pinia

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col justify-center items-center h-full my-40">
+  <div class="flex flex-col justify-center items-center h-screen my-40">
     <h1 class="mb-4">home</h1>
     <Toggle />
   </div>
