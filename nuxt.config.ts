738a1102837dba55b9a0a6daa03e76387f3c14f3
Joshua <afekurojoshua@gmail.com>
2023-08-25 17:16:06 +0100

    add css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Demo',
+      link: [
+        { rel: 'stylesheet', href: 'https://use.typekit.net/nno1nqe.css' },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.png',
+        },
+      ],
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  },
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
